7a05f514860a2ca34c8b43a81baadf68f979b26c
koizumin <koizumi.ryo.dev@gmail.com>
2018-01-14 17:52:53 +0900

    use `match` instead of `if ~ else if ~`

--- scala-tour/src/main/scala/example/FizzBuzz.scala
+++ scala-tour/src/main/scala/example/FizzBuzz.scala
@@ -8,14 +8,11 @@ object FizzBuzz {
   def main(args: Array[String]): Unit = { // println の戻り値は Unit
     val n = 20 // val は定数
     for { i <- 1 to n } {
-      if (i % 15 == 0) {
-        println("FizzBuzz")
-      } else if (i % 3 == 0) {
-        println("Fizz")
-      } else if (i % 5 == 0) {
-        println("Buzz")
-      } else {
-        println(i)
+      i match {
+        case x if x % 15 == 0 => println("FizzBuzz")
+        case x if x % 3 == 0 => println("Fizz")
+        case x if x % 5 == 0 => println("Buzz")
+        case x => println(x)
       }
     }
     // scala では最後に実行された式が評価された値がブロックの値となる
