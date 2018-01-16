c370ce662015172348e696dcbc035da22f1eed22
koizumin <koizumi.ryo.dev@gmail.com>
2018-01-16 13:59:22 +0900

    lean case class

--- scala-tour/src/main/scala/example/CaseClass.scala
+++ scala-tour/src/main/scala/example/CaseClass.scala
@@ -7,11 +7,37 @@ package example
 
 object CaseClass {
   def main(args: Array[String]): Unit = {
-
-      // apply メソッドは
+    // apply メソッドは
     val foo = new Foo
     // foo() // 引数なしなのに () つけて呼び出してるからかコンパイルエラー
     println(Foo("argument"))
+
+    // コンストラクタ引数の扱い
+    val generator = new scala.util.Random
+    case class Bar(i: Int) { // case class のコンストラクタ引数は public な val になる
+      val randomValue = generator.nextInt
+    }
+    val bar = Bar(10) // == Foo.apply(10)
+    println(bar.i) // アクセスできる
+
+    // 文字列に変換したときの違い
+    class Baz
+    val baz = new Baz
+    println(baz) // 普通のクラスだとハッシュ値
+    println(baz.toString)
+    println(bar) // case class だと良い感じにオブジェクトを表示してくれる
+    println(bar.toString)
+
+    // コピー時の挙動
+    val barCopy = bar.copy()
+    println(s"${bar}, i: ${bar.i}, random: ${bar.randomValue}")
+    println(s"${barCopy}, i: ${barCopy.i}, random: ${barCopy.randomValue}")
+
+    // パターンマッチが便利
+    bar match {
+      case Bar(i) => println(s"class Bar, i: ${i}") // マッチしたときにパラメータをキャプチャできる
+      case _ => println("not class Bar")
+    }
   }
 }
 
