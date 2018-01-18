47fd7d1949540e9894f58498a3de4c046fabb939
koizumin <koizumi.ryo.dev@gmail.com>
2018-01-18 13:50:44 +0900

    get enum object’s name with case keyword

--- scala-tour/src/main/scala/example/Enum.scala
+++ scala-tour/src/main/scala/example/Enum.scala
@@ -7,23 +7,17 @@ package example
 
 sealed abstract class Animal(val cryJP: String, val cryEN: String) // sealed は同じファイル内でのみ継承できるアクセス修飾子
 // これ以上継承されない Animal を親としたオブジェクトを作成することで列挙のように扱うことができる
-object Cat extends Animal("にゃー", "Meow")
+case object Cat extends Animal("にゃー", "Meow")
 
-object Dog extends Animal("わんわん", "Bowwow")
+case object Dog extends Animal("わんわん", "Bowwow")
 
-object Lion extends Animal("がおー", "Roar")
+case object Lion extends Animal("がおー", "Roar")
 
 object Enum {
   def main(args: Array[String]): Unit = {
     for {animal <- List(Lion, Cat, Dog)} {
-      animal match {
-        case Cat =>
-          println(s"Cat ${animal.cryJP} ${animal.cryEN}")
-        case Dog =>
-          println(s"Dog ${animal.cryJP} ${animal.cryEN}")
-        case Lion =>
-          println(s"Lion ${animal.cryJP} ${animal.cryEN}")
-      }
+      // 列挙を case object で作ることによって良い感じに名前が表示される
+      println(s"${animal}, ${animal.cryJP} ${animal.cryEN}")
     }
   }
 }
