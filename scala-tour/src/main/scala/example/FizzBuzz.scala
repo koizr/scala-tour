be32082630a07da8fd9dc8b826ea2fca32d1bb20
koizumin <koizumi.ryo.dev@gmail.com>
2018-01-22 08:55:36 +0900

    meet fizzbuzz again

--- scala-tour/src/main/scala/example/FizzBuzz.scala
+++ scala-tour/src/main/scala/example/FizzBuzz.scala
@@ -10,6 +10,8 @@ object FizzBuzz {
   def main(args: Array[String]): Unit = {
     val n = 20 // val は定数
     fizzbuzz(n)
+    fizzbuzzAgain(n)
+    fizzBuzzAgainAgain(n)
   }
 
   /**
@@ -29,5 +31,28 @@ object FizzBuzz {
 
     // 末尾再帰最適化 ってのがあるので最後に評価される式が再帰じゃないと最適化されない
     if (i < n) fizzbuzz(n, i + 1)
+  }
+
+  def fizzbuzzAgain(n: Int): Unit = {
+    // args => return value の書き方でラムダ式になる
+    // Scala の場合、メソッドと関数は明確に別れており、関数は第一級の値であるがメソッドは第一級ではない
+    (1 to n).foreach(i => i match {
+      case x if x % 15 == 0 => println("FizzBuzz")
+      case x if x % 3 == 0 => println("Fizz")
+      case x if x % 5 == 0 => println("Buzz")
+      case x => println(x.toString)
+    })
+  }
+
+  def fizzBuzzAgainAgain(n: Int): Unit = {
+    val printFizzBuzz = (i: Int) => i match {
+      case x if x % 15 == 0 => println("FizzBuzz")
+      case x if x % 3 == 0 => println("Fizz")
+      case x if x % 5 == 0 => println("Buzz")
+      case x => println(x.toString)
+    }
+
+    // 中置記法
+    1 to n foreach printFizzBuzz
   }
 }
